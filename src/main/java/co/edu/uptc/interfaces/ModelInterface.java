35c582f2e57c9fa854efd949bbef43fd9a0156bb
AlexisTrey <yulian.tobar13@uptc.edu.co>
2026-05-06 17:51:00 -0500

    Actualización de Cambios

--- src/main/java/co/edu/uptc/interfaces/ModelInterface.java
+++ src/main/java/co/edu/uptc/interfaces/ModelInterface.java
@@ -8,32 +8,18 @@ import java.util.List;
 public interface ModelInterface {
 
     void startGame();
-
     void pauseGame();
-
     void resumeGame();
-
     void resetGame();
-
     void addBall();
-
     void moveRacket(int y);
-
     void moveRacketBy(int deltaY);
-
     void increaseSpeed();
-
     void decreaseSpeed();
-
     List<Ball> getBalls();
-
     Racket getRacket();
-
     boolean isGameOver();
-
     boolean isPaused();
-
     String getStartTime();
-
     long getElapsedSeconds();
 }
--- src/main/java/co/edu/uptc/interfaces/PresenterInterface.java
+++ src/main/java/co/edu/uptc/interfaces/PresenterInterface.java
@@ -1,29 +1,19 @@
 package co.edu.uptc.interfaces;
 
 public interface PresenterInterface {
+
     void setModel(ModelInterface model);
-
     void setView(ViewInterface view);
-
     void onStartGame();
-
     void onPauseGame();
-
     void onResumeGame();
-
     void onResetGame();
-
     void onAddBall();
-
     void onMoveRacket(int y);
-
     void onMoveRacketBy(int deltaY);
-
     void onIncreaseSpeed();
-
     void onDecreaseSpeed();
-
     void refreshView();
-
     void onGameOver();
+
 }

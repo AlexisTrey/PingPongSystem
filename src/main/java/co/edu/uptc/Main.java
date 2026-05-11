dcb3cb92d53458405b5455a609dcba6d346e7ab8
AlexisTrey <yulian.tobar13@uptc.edu.co>
2026-05-11 16:53:09 -0500

    Actualización de Cambios

--- src/main/java/co/edu/uptc/Main.java
+++ src/main/java/co/edu/uptc/Main.java
@@ -3,11 +3,18 @@ package co.edu.uptc;
 import co.edu.uptc.presenter.Runner;
 import co.edu.uptc.util.ThemeManager;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        ThemeManager.applyByKey(ThemeManager.LIGHT);
-        new Runner().start();
+
+        SwingUtilities.invokeLater(() -> {
+
+            ThemeManager.applyByKey(ThemeManager.LIGHT);
+
+            new Runner().start();
+        });
     }
 
 }
